c781f3d2449010f042d3c2c8925dfbfc645d6d81
JaydenDev <willsjayden2@gmail.com>
2022-04-23 12:44:52 -0500

    init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="nav">
+  <ul>
+    <li><h1>MyScratchPage</h1></li>
+  </ul>
+  </div>
+  <input class="input-box" type="text" v-model="search" placeholder="User">
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -19,8 +21,31 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+  background-color: #111;;
+}
+*{
+  margin:0;
+  padding:0;
+}
+body {
+  top: 0;
+  position: relative;
+  background-color: #111;;
+}
+#nav {
+  background-color: #222;
+  list-style-type: none;
+  padding: 3pt;
+}
+ul {
+  list-style-type: none;
+  display: flex;
+}
+.input-box {
+  width: 100%;
+  padding: 6pt;
+  border: none;
+  outline: none;
 }
 </style>
